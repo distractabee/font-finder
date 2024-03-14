67db7f32639d666bbf290142a42b31a122238f62
distractabee <jssclgh@gmail.com>
2024-03-14 18:53:21 -0500

    added files for the post and comment handlebars

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -7,7 +7,7 @@ const { User, Post, Comment } = require('../../models');
 router.post('/', async (req, res) => {
     try {
         const newPost = await Post.create({
-            title: req.body.title,
+            title: req.body.post_title,
             post_body: req.body.post_body,
             user_id: req.session.user_id || req.body.user_id
         });
@@ -34,25 +34,6 @@ router.delete('/:id', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     } 
-});
-
-// update post
-router.put('/:id', async (req, res) => {
-    try {
-        const updatedPost = await Post.update(req.body, {
-            where: {
-                id: req.params.id
-            }
-        });
-        if (!updatedPost[0]) {
-            res.status(400).json({ message: "No post found with that id!" });
-            return;
-        }
-        res.status(200).json(updatedPost);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json(err);
-    }
 });
 
 // get single post by ID
