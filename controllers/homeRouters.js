abdc7a99d78b0cfbd3360bc92554a4fe7881d244
distractabee <jssclgh@gmail.com>
2024-03-13 19:15:07 -0500

    updated index.js

--- controllers/homeRouters.js
+++ controllers/homeRouters.js
@@ -62,7 +62,7 @@ router.get('/profile', withAuth, async (req, res) => {
         const user = fontData.get({ plain: true });
 
         res.render('profile', {
-            ..user,
+            ...user,
             logged_in: true
         });
     } catch (err) {
