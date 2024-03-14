d54b4790805b91ec8a12fd3c85696d7a4483bd4f
distractabee <jssclgh@gmail.com>
2024-03-14 14:52:53 -0500

    added a model for blog posts

---
+++ models/Fonts.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Fonts extends Model {}
+
+Fonts.init(
+  {
+    font_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    font_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    font_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    is_serif: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'font',
+  }
+);
+
+module.exports = Fonts;
---
+++ models/Post.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Post extends Model {}
+
+Post.init(
+    {
+      post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      post_title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+      },
+      post_body: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+    }, 
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
